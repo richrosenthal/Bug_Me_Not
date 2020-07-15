d130ab8c766bf1af6dcfbeddb5c64289fcb9422b
richrosenthal <rrosenthal2013@gmail.com>
2020-07-14 21:40:35 -0400

    add strong params method to userscontroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,16 @@ class UsersController < ApplicationController
 
   #signup users
   def create
+
+    @user.save
+  end
+
+  #private method
+  private
+
+ #adds strong params <- Don't forget this again....for real 
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 
 end
