0c0d4c72426c8bd02a33cec51eb90da02506b62c
richrosenthal <rrosenthal2013@gmail.com>
2020-07-19 22:42:36 -0400

    update associations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-  has_secure_password
+  has_many :updates
+  has_many :updated_tickets, through: :updates, source: :ticket
+  has_many :tickets #that the user created
 
+  has_secure_password
   validates :username, presence: true
-  validates :username, uniqueness: true 
+  validates :username, uniqueness: true
 end
