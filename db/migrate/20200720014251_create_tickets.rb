65550ce199167daad59f4a924e9d29f7ae36fd6b
richrosenthal <rrosenthal2013@gmail.com>
2020-07-19 21:43:18 -0400

    generate "ticket" resource

---
+++ db/migrate/20200720014251_create_tickets.rb
@@ -0,0 +1,12 @@
+class CreateTickets < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tickets do |t|
+      t.string :title
+      t.string :description
+      t.belongs_to :project, null: false, foreign_key: true
+      t.belongs_to :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
