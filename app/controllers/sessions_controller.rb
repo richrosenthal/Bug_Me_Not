389f7bc0311ae120a1a2fbcb7c45fd851d948757
richrosenthal <rrosenthal2013@gmail.com>
2020-07-20 00:00:14 -0400

    add create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,7 @@
 class SessionsController < ApplicationController
    def new
      @user = User.new
-     render :login 
-   end
-
-   def create
+     render :login
    end
 
    def home
@@ -13,5 +10,18 @@ class SessionsController < ApplicationController
    def destroy
     session.clear
     redirect_to '/'
+   end
+
+   def create
+     #does user exist?
+     @user = User.find_by(username: params[:user][:username])
+     #was user found? was password correct?
+     if @user.try(:authenticate, params[:user][:password])
+       session[:user_id] = @user.id
+       redirect_to user_path(@user)
+     else
+       flash[:error] = "Either username or password was incorrect. Please try again"
+       redirect_to login_path
+     end
    end
 end
