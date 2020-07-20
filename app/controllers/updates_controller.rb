bed9e1864e15cc3c5c7d87b61cd212d3795365df
richrosenthal <rrosenthal2013@gmail.com>
2020-07-20 13:45:22 -0400

    added several actions to UpdatesController

--- app/controllers/updates_controller.rb
+++ app/controllers/updates_controller.rb
@@ -1,2 +1,29 @@
 class UpdatesController < ApplicationController
+  def new
+    @ticket = Ticket.find_by_id(params[:ticket_id])
+    @update = @ticket.updates.build
+  end
+
+  def create
+    @update = current_user.updates.build(update_params)
+    if @update.save
+      redirect_to update_path(@update)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @update = Update.find_by_id(params[:id])
+  end
+
+  def index
+  end
+
+  private
+
+  def update_params
+    params.require(:update).permit(:ticket_id, :content)
+  end
+
 end
