f09f627eab95074519e556163e0e90ebe6285723
richrosenthal <rrosenthal2013@gmail.com>
2020-07-19 22:42:47 -0400

    update associations for ticket model

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,7 @@
 class Ticket < ApplicationRecord
   belongs_to :project
-  belongs_to :user
+  belongs_to :user #creator of the ticket
+  has_many :updates
+  has_many :users, through: :updates #users who have submitted updates to the tickets
+
 end
