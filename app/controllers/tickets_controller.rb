04f2a3ecde5c9c90e6b3bd5b314a93baa7db0ff4
richrosenthal <rrosenthal2013@gmail.com>
2020-07-20 00:27:52 -0400

    add actions to tickets controller

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,24 @@
 class TicketsController < ApplicationController
+
+  def new
+    @ticket = Ticket.new
+    @ticket.build_project
+  end
+
+  def create
+    @ticket = Ticket.new(ticket_params)
+    @ticket.user_id = session[:id]
+    if @ticket.save
+      redirect_to ticket_path(@ticket)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def ticket_params
+    params.require(:ticket).permit(:title, :description, :project_id, project_attributes: [:name])
+  end
+
 end
