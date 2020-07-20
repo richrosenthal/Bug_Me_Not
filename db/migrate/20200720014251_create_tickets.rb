d6a65cfeca59d1b0efb93b63acaa2f6a907f2381
richrosenthal <rrosenthal2013@gmail.com>
2020-07-19 21:47:14 -0400

    add is_complete column to migration

--- db/migrate/20200720014251_create_tickets.rb
+++ db/migrate/20200720014251_create_tickets.rb
@@ -3,6 +3,7 @@ class CreateTickets < ActiveRecord::Migration[6.0]
     create_table :tickets do |t|
       t.string :title
       t.string :description
+      t.boolean :is_complete
       t.belongs_to :project, null: false, foreign_key: true
       t.belongs_to :user, null: false, foreign_key: true
 
