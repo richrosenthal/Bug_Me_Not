2d8eef23d40c088e7de49ac655228b39c0f238d1
richrosenthal <rrosenthal2013@gmail.com>
2020-07-20 00:28:32 -0400

    add the ability to accept nested attributes for project model

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,5 +3,5 @@ class Ticket < ApplicationRecord
   belongs_to :user #creator of the ticket
   has_many :updates
   has_many :users, through: :updates #users who have submitted updates to the tickets
-
+  accepts_nested_attributes_for :project 
 end
