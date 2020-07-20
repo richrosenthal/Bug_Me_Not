43679264c9cfe86fac35c94755f3fefae3e281d0
richrosenthal <rrosenthal2013@gmail.com>
2020-07-20 17:43:10 -0400

    add validation

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,5 +3,9 @@ class Ticket < ApplicationRecord
   belongs_to :user #creator of the ticket
   has_many :updates
   has_many :users, through: :updates #users who have submitted updates to the tickets
-  accepts_nested_attributes_for :project 
+  accepts_nested_attributes_for :project
+
+  validates :title, presence: true
+  validates :description, presence: true
+
 end
